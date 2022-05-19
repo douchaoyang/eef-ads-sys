08fe9c21587d1a0578f2c5cfd060145f6304c180
douchaoyang <chaoyang.dou@cn.supplyframe.com>
2022-05-19 09:38:44 +0800

    update git

---
+++ dist/static/js/chunk-2d0e64b9.97001b2c.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0e64b9"],{"97af":function(n,e,t){"use strict";t.r(e);var a=function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("div",{staticClass:"app-container"},[n._v("banner edit")])},c=[],s={},i=s,r=t("2877"),u=Object(r["a"])(i,a,c,!1,null,"34f47bd4",null);e["default"]=u.exports}}]);
